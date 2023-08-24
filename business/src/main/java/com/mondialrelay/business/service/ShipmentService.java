9b2de4e005cd80d70c4043a467210c5e6402e380
DEHANI Djamel <ddehani@mondialrelay.fr>
2023-08-24 17:30:30 +0200

    Create transaction pissue

--- business/src/main/java/com/mondialrelay/business/service/ShipmentService.java
+++ business/src/main/java/com/mondialrelay/business/service/ShipmentService.java
@@ -3,6 +3,7 @@ package com.mondialrelay.business.service;
 import com.mondialrelay.business.dto.ParcelDto;
 import com.mondialrelay.business.dto.ShipmentDto;
 import com.mondialrelay.business.mapper.ShipmentMapper;
+import com.mondialrelay.data.entity.Parcel;
 import com.mondialrelay.data.entity.Shipment;
 import com.mondialrelay.data.repository.ShipmentRepository;
 import jakarta.persistence.EntityNotFoundException;
@@ -13,6 +14,7 @@ import java.util.Collection;
 
 import static java.util.stream.Collectors.toList;
 
+
 @Service
 @AllArgsConstructor
 public class ShipmentService {
@@ -21,32 +23,83 @@ public class ShipmentService {
     private ParcelService parcelService;
     private ShipmentMapper mapper;
 
-    private Shipment getShipmentById(Long id) {
+    /**
+     * Retrieves a shipment by its ID.
+     *
+     * @param id The ID of the shipment.
+     * @return The shipment with the specified ID.
+     * @throws EntityNotFoundException If the shipment is not found.
+     */
+    private Shipment getShipmentById(final Long id) {
         return repository.findById(id).orElseThrow(() -> new EntityNotFoundException("Shipment not found with id: " + id));
     }
 
+    /**
+     * Retrieves a list of all shipments.
+     *
+     * @return A collection of shipment DTOs.
+     */
     public Collection<ShipmentDto> findShipments() {
         return repository.findAll().stream().map(mapper::map).collect(toList());
     }
 
+    /**
+     * Creates a new shipment.
+     *
+     * @param dto The DTO containing shipment information.
+     * @return The DTO of the newly created shipment.
+     */
     public ShipmentDto createShipment(final ShipmentDto dto) {
         final var shipment = mapper.map(dto);
         final var entity = repository.save(shipment);
         return mapper.map(entity);
     }
 
-    public ShipmentDto updateShipment(Long id, ShipmentDto updatedShipmentDto) {
+    /**
+     * Updates an existing shipment.
+     *
+     * @param id                 The ID of the shipment to update.
+     * @param updatedShipmentDto The DTO containing updated shipment information.
+     * @return The DTO of the updated shipment.
+     * @throws IllegalArgumentException If any of the parcel in {@code updatedShipmentDto} are not linked to the shipment.
+     */
+    public ShipmentDto updateShipment(final Long id, final ShipmentDto updatedShipmentDto) {
         final var existingShipment = getShipmentById(id);
         final var updatedShipment = mapper.map(updatedShipmentDto);
         existingShipment.setTrackingNumber(updatedShipment.getTrackingNumber());
-        return mapper.map(repository.save(existingShipment));
+        final var dto = mapper.map(repository.save(existingShipment));
+        // Validate and update parcels
+        validateParcelIdsLinkedToShipment(existingShipment.getParcels(), updatedShipmentDto.getParcels());
+        dto.setParcels(parcelService.updateParcels(updatedShipmentDto.getParcels()));
+        return dto;
     }
 
-    public void deleteShipment(Long id) {
+    private void validateParcelIdsLinkedToShipment(final Collection<Parcel> existingParcels, final Collection<ParcelDto> updatedParcels) {
+        final var isInvalidParcel = updatedParcels.stream()
+                .anyMatch(updatedParcel -> existingParcels.stream()
+                        .noneMatch(existingParcel -> existingParcel.getId().equals(updatedParcel.getId())));
+        if (isInvalidParcel) {
+            throw new IllegalArgumentException("One or more parcels are not linked to the shipment.");
+        }
+    }
+
+    /**
+     * Deletes a shipment by its ID.
+     *
+     * @param id The ID of the shipment to delete.
+     */
+    public void deleteShipment(final Long id) {
         final var shipment = getShipmentById(id);
         repository.delete(shipment);
     }
 
+    /**
+     * Creates a new parcel for a specific shipment.
+     *
+     * @param shipmentId The ID of the shipment to which the parcel belongs.
+     * @param dto        The DTO containing parcel information.
+     * @return The DTO of the newly created parcel.
+     */
     public ParcelDto createParcelForShipment(final Long shipmentId, final ParcelDto dto) {
         final var shipment = getShipmentById(shipmentId);
         return parcelService.createParcel(dto, shipment);
--- webservice/src/main/java/com/mondialrelay/interview/controller/ShipmentController.java
+++ webservice/src/main/java/com/mondialrelay/interview/controller/ShipmentController.java
@@ -19,27 +19,58 @@ public class ShipmentController {
 
     private ShipmentService shipmentService;
 
+    /**
+     * Retrieves a collection of all shipments.
+     *
+     * @return A collection of shipment DTOs.
+     */
     @GetMapping
     public Collection<ShipmentDto> findShipments() {
         return shipmentService.findShipments();
     }
 
+    /**
+     * Creates a new shipment.
+     *
+     * @param dto The DTO containing shipment information.
+     * @return A ResponseEntity containing the created shipment DTO.
+     */
     @PostMapping
     public ResponseEntity<ShipmentDto> createShipment(final @RequestBody ShipmentDto dto) {
         return ResponseEntity.status(CREATED).body(shipmentService.createShipment(dto));
     }
 
+    /**
+     * Updates an existing shipment.
+     *
+     * @param id       The ID of the shipment to update.
+     * @param shipment The updated shipment information.
+     * @return A ResponseEntity containing the updated shipment DTO.
+     */
     @PutMapping("/{id}")
     public ResponseEntity<ShipmentDto> updateShipment(@PathVariable final Long id, @RequestBody final ShipmentDto shipment) {
         return ResponseEntity.ok(shipmentService.updateShipment(id, shipment));
     }
 
+    /**
+     * Deletes a shipment by its ID.
+     *
+     * @param id The ID of the shipment to delete.
+     * @return A ResponseEntity with no content.
+     */
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteShipment(@PathVariable final Long id) {
         shipmentService.deleteShipment(id);
         return ResponseEntity.noContent().build();
     }
 
+    /**
+     * Creates a new parcel for a specific shipment.
+     *
+     * @param shipmentId The ID of the shipment for which to create the parcel.
+     * @param parcel     The DTO containing parcel information.
+     * @return A ResponseEntity containing the created parcel DTO.
+     */
     @PostMapping("/{shipmentId}/parcels")
     public ResponseEntity<ParcelDto> createParcelForShipment(
             @PathVariable Long shipmentId,
